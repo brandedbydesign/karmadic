6db2fa316d19d1e70d477ed2c3a0716fb29efb3f
Brandy Fetzner <brande24@brandedbydesign.net>
2019-06-12 16:17:41 -0400

    Modified header.php

--- header.php
+++ header.php
@@ -6,7 +6,8 @@
  *
  * @package karmadic
  */
-?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
@@ -14,55 +15,61 @@
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-
+<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
+<script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
 <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="hfeed site">
-	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'karmadic' ); ?></a>
+	<div id="page" class="hfeed site">
+		<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'karmadic' ); ?></a>
 
-	<header id="masthead" class="site-header">
-		<div class="container">
-
-			<div class="row">
-				<div class="col-md-6">
-					<div id="logo"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+		<header id="masthead" class="site-header">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-6">
+						<div id="logo"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
 						<img src="<?php echo get_template_directory_uri(); ?>/images/logo.png" alt="company logo" /></a>
-						<!--<h2 class="site-description"><?php // bloginfo( 'description' ); ?></h2>-->
-					</div><!-- #logo -->
-				</div><!-- .col -->
-			</div><!-- .row -->
-
-			<div class="row">
-				<nav id="site-navigation" class="navbar navbar-default">
-					<div class="navbar-header">
-                    	<!--<a class="navbar-brand visible-xs" href="#">Main Menu</a>-->
-						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#primary-navbar">
+							<!--<h2 class="site-description"><?php // bloginfo( 'description' ); ?></h2>-->
+						</div><!-- #logo -->
+					</div><!-- .col -->
+					<div class="col-sm-6">
+						<div class="header-widget">
+							<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('header-widget') ) : endif; ?>
+						</div><!-- .header-widget -->
+					</div><!-- .col -->
+				</div><!-- .row -->
+			</div><!-- .container -->
+		<nav id="site-navigation" class="navbar navbar-default">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-12">
+						<div class="navbar-header">
+							<!--<a class="navbar-brand visible-xs" data-toggle="collapse" data-target="#primary-navbar" href="#">Main Menu</a>-->
+							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#primary-navbar">
 							<span class="sr-only">Toggle navigation</span>
                             <span class="icon-bar"></span>
 							<span class="icon-bar"></span>
 							<span class="icon-bar"></span>
 						</button>
-					</div><!-- .navbar-header -->
+						</div><!-- .navbar-header -->
+						<?php wp_nav_menu( array(
+							'menu'              => 'primary',
+							'theme_location'    => 'primary',
+							'depth'             => 3,
+							'container'         => 'div',
+							'container_class'   => 'collapse navbar-collapse',
+							'container_id'      => 'primary-navbar',
+							'menu_class'        => 'nav navbar-nav',
+							'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+							'walker'            => new wp_bootstrap_navwalker())
+							);
+						?>
+					</div><!-- .col -->
+				</div><!-- .row -->
+			</div><!-- .container -->
+		</nav><!-- #site-navigation -->
+		</header><!-- .site-header -->
 
-					<?php wp_nav_menu( array(
-						'menu'              => 'primary',
-						'theme_location'    => 'primary',
-						'depth'             => 3,
-						'container'         => 'div',
-						'container_class'   => 'collapse navbar-collapse',
-						'container_id'      => 'primary-navbar',
-						'menu_class'        => 'nav navbar-nav',
-						'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
-						'walker'            => new wp_bootstrap_navwalker())
-						);
-					?>
-				</nav><!-- #site-navigation -->
-			</div><!-- .row -->
-
-		</div><!-- .container -->
-	</header><!-- .site-header -->
-
-	<div id="content" class="site-content">
-		<div class="container">
+		<div id="content" class="site-content">
+			<div class="container">
