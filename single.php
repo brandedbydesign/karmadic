ed138f1b436764dc66eb8a9904f8d10a461d1835
Brandy Fetzner <brandyfetzner@gmail.com>
2019-06-15 14:01:04 -0400

    Update single.php

--- single.php
+++ single.php
@@ -6,37 +6,32 @@
  */
 
 get_header(); ?>
-
+</div><!-- end container -->
+<div class="leaderboard-image">
+		
+</div><!-- end leaderboard-image -->
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
-		<div class="row">
-
-				<div class="col-md-3">
-				<?php get_sidebar(1); ?>
-
-				</div><!-- .col -->
-
-
-
-				<div class="col-md-9">
-		<?php while ( have_posts() ) : the_post(); ?>
-
-			<?php get_template_part( 'content', 'single' ); ?>
-
-			<?php karmadic_post_nav(); ?>
-
-			<?php
-				// If comments are open or we have at least one comment, load up the comment template
-				if ( comments_open() || '0' != get_comments_number() ) :
-					comments_template();
-				endif;
-			?>
-
-		<?php endwhile; // end of the loop. ?>
-</div><!-- .col -->
-
-			</div><!-- .row -->
+			<div class="container">
+				<div class="row">
+					<div class="col-md-3">
+						<?php get_sidebar(1); ?>
+					</div><!-- .col -->
+					<div class="col-md-9">
+						<?php while ( have_posts() ) : the_post(); ?>
+							<?php get_template_part( 'content', 'single' ); ?>
+							<?php yourweblayout_post_nav(); ?>
+							<?php
+								// If comments are open or we have at least one comment, load up the comment template
+								if ( comments_open() || '0' != get_comments_number() ) :
+									comments_template();
+								endif;
+							?>
+						<?php endwhile; // end of the loop. ?>
+					</div><!-- .col -->
+				</div><!-- .row -->
+			</div><!-- end container -->
 		</main><!-- #main -->
 	</div><!-- #primary -->
-
+<div class="container">
 <?php get_footer(); ?>
